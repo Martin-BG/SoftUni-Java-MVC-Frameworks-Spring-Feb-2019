415f0b69a75441cffb19a6fb6c588d383a3f3e54
MartinBG <MartinBG@abv.bg>
2019-03-05 21:34:16 +0200

    app entry point

---
+++ 01. Spring Boot Introduction/Exercises/Real Estate Agency/src/main/java/org/softuni/realestate/RealEstateApplication.java
@@ -0,0 +1,12 @@
+package org.softuni.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateApplication.class, args);
+    }
+}
