8608290481ad66120658afe7f268929f3d74cfd8
MartinBG <MartinBG@abv.bg>
2019-03-05 21:47:17 +0200

    placeholder

---
+++ 01. Spring Boot Introduction/Exercises/Real Estate Agency/src/test/java/org/softuni/realestate/RealEstateApplicationTests.java
@@ -0,0 +1,16 @@
+package org.softuni.realestate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealEstateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
