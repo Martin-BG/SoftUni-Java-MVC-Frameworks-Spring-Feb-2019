42c61ca26f4e75223906beef2ffef6f98b8846cc
MartinBG <MartinBG@abv.bg>
2019-03-07 15:51:03 +0200

    javadoc

--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/config/MySQL8UnicodeDialect.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/config/MySQL8UnicodeDialect.java
@@ -2,6 +2,18 @@ package org.softuni.exodia.config;
 
 import org.hibernate.dialect.MySQL8Dialect;
 
+/**
+ * Custom MySQL dialect settings:
+ * <ul>
+ * <li>engine: InnoDB</li>
+ * <li>charset: utf8mb4</li>
+ * <li>collate: utf8mb4_unicode_ci</li>
+ * </ul>
+ * <p>
+ * Set in application.properties:
+ * <p>
+ * spring.jpa.properties.hibernate.dialect=my.package.MySQL8UnicodeDialect
+ */
 public class MySQL8UnicodeDialect extends MySQL8Dialect {
 
     @Override
