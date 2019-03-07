7e577a47d7755b76dec09086113720d841674acb
MartinBG <MartinBG@abv.bg>
2019-03-07 15:38:45 +0200

    Base UUID entity class

--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/domain/entities/BaseUuidEntity.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/domain/entities/BaseUuidEntity.java
@@ -1,17 +1,36 @@
 package org.softuni.exodia.domain.entities;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
-import org.softuni.exodia.domain.api.Identifiable;
 
 import javax.persistence.*;
 import java.util.UUID;
 
-@Setter
+/**
+ * {@link UUID} implementation of {@link BaseEntity} abstract class
+ * <p>
+ * Use space optimized BINARY(16) instead of CHAR/VARCHAR(36) database format
+ * <p>
+ * Use AccessType.PROPERTY for id as best practice to avoid LazyInitializationException
+ * <p>
+ * Use private setter to prevent mutability
+ * <ul>
+ * Useful SQL commands:
+ * <li>
+ * Select by id:
+ * <br>
+ * SELECT * FROM table_name where id = UUID_TO_BIN('24abff5d-f3d9-46df-a5ad-361fcbfe045e');
+ * <li>
+ * List IDs as UUID:
+ * <br>
+ * SELECT BIN_TO_UUID(id) FROM table_name
+ */
+@Setter(AccessLevel.PRIVATE)
 @Getter
 @MappedSuperclass
-abstract class BaseUuidEntity implements Identifiable<UUID> {
+abstract class BaseUuidEntity extends BaseEntity<UUID> {
 
     @Id
     @GeneratedValue(generator = "uuid2")
