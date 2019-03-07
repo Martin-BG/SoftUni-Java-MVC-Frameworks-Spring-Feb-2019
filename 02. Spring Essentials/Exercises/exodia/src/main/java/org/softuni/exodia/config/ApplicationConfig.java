ce80c761c6c29f1837b0f97727922e75a468fd69
MartinBG <MartinBG@abv.bg>
2019-03-07 13:51:56 +0200

    App config - ModelMapper customized bean (work on fields instead with getters/setters) and change system TimeZone to UTC to match DB setting

---
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/config/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package org.softuni.exodia.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    ModelMapper createModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setFieldMatchingEnabled(true);
+
+        return modelMapper;
+    }
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+}
