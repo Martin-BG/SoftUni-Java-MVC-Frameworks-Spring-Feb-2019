3ca984d20fb04117513a0942fc863a2b3d7983d1
MartinBG <MartinBG@abv.bg>
2019-03-07 13:49:18 +0200

    initial project structure

---
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/ExodiaApplication.java
@@ -0,0 +1,29 @@
+package org.softuni.exodia;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.logging.Level;
+
+@Log
+@SpringBootApplication
+public class ExodiaApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExodiaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExodiaApplication.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.log(Level.INFO, "Application deployed and ready to work");
+    }
+}
---
+++ 02. Spring Essentials/Exercises/exodia/src/test/java/org/softuni/exodia/ExodiaApplicationTests.java
@@ -0,0 +1,16 @@
+package org.softuni.exodia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExodiaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
