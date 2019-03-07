4f2baa5fc7d01dbb96ae12cf5a44810e8113ffb3
MartinBG <MartinBG@abv.bg>
2019-03-07 16:54:10 +0200

    javadoc formatting

--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/domain/entities/BaseEntity.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/domain/entities/BaseEntity.java
@@ -4,9 +4,9 @@ import org.softuni.exodia.domain.api.Identifiable;
 
 /**
  * Base Entity class
- * Defines equals() and hashCode() methods according
- * to best practices suggested by Vlad Mihalcea
- * @see <a href="https://vladmihalcea.com/the-best-way-to-implement-equals-hashcode-and-tostring-with-jpa-and-hibernate/">The best way to implement equals, hashCode, and toString with JPA and Hibernate</a>
+ * Defines equals() and hashCode() methods according to best practices by Vlad Mihalcea
+ * @see <a href="https://vladmihalcea.com/the-best-way-to-implement-equals-hashcode-and-tostring-with-jpa-and-hibernate/">
+ *     The best way to implement equals, hashCode, and toString with JPA and Hibernate</a>
  */
 abstract class BaseEntity<I> implements Identifiable<I> {
 
