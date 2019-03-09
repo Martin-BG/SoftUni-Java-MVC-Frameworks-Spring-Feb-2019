75fd25411b43745d625045be00f17ff43092153c
MartinBG <MartinBG@abv.bg>
2019-03-10 01:37:25 +0200

    Add redirect url() and sessionAttribute() attributes to AuthenticatedUser annotation

--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/annotations/AuthenticatedUser.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/annotations/AuthenticatedUser.java
@@ -12,11 +12,24 @@ import java.lang.annotation.Target;
  * <p>
  * Can be placed on Controller class or methods (higher priority).
  * <p>
+ * url is optional and defines the URL to redirect invalid requests to
+ * otherwise, URLs defined by {@link AuthenticatedInterceptor} will be used
+ * <p>
+ * sessionAttribute is optional and allows to check for another Session Attribute,
+ * instead of the default one defined by {@link AuthenticatedInterceptor}
+ * <p>
  * Enforced by {@link AuthenticatedInterceptor}
  */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface AuthenticatedUser {
+    String USE_DEFAULT_REDIRECT_URL = "";
+    String USE_DEFAULT_SESSION_ATTRIBUTE_NAME = "";
 
     boolean authenticated() default true;
+
+    String url() default USE_DEFAULT_REDIRECT_URL;
+
+    String sessionAttribute() default USE_DEFAULT_SESSION_ATTRIBUTE_NAME;
 }
