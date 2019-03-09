f892401201913c858ddcfd29bf7f9a4fc7d80344
MartinBG <MartinBG@abv.bg>
2019-03-09 06:50:23 +0200

    base controller

---
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/web/controllers/BaseController.java
@@ -0,0 +1,33 @@
+package org.softuni.exodia.web.controllers;
+
+import lombok.extern.java.Log;
+import org.springframework.ui.Model;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Level;
+
+@Log
+class BaseController {
+
+    protected BaseController() {
+    }
+
+    protected static Optional<UUID> uuid(String id) {
+        try {
+            return Optional.of(UUID.fromString(id));
+        } catch (IllegalArgumentException e) {
+            log.log(Level.SEVERE, "String cannot be converted to UUID: " + id, e);
+            return Optional.empty();
+        }
+    }
+
+    protected String buildView(String view, Model model) {
+        model.addAttribute("view", "views/" + view + ".html");
+        return "base-layout";
+    }
+
+    protected String redirect(String url) {
+        return "redirect:" + url;
+    }
+}
