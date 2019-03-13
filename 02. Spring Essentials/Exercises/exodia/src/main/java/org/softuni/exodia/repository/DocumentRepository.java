497e6757daf81906cd5f9530f9ba4f1acb855fb9
MartinBG <MartinBG@abv.bg>
2019-03-13 11:39:36 +0200

    enable repository methods parameters validation - @Validated

--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/repository/DocumentRepository.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/repository/DocumentRepository.java
@@ -4,11 +4,13 @@ import org.softuni.exodia.domain.entities.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 
 import javax.persistence.Tuple;
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, UUID> {
 
--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/repository/UserRepository.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/repository/UserRepository.java
@@ -3,12 +3,14 @@ package org.softuni.exodia.repository;
 import org.softuni.exodia.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Optional;
 import java.util.UUID;
 
+@Validated
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
