ce655ba11a1e4ba519f79dc4f08b581d540b6dc1
MartinBG <MartinBG@abv.bg>
2019-03-14 03:56:23 +0200

    add Serializable interface to DocumentDetailsViewModel.class (saved in session)

--- 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/domain/models/view/document/DocumentDetailsViewModel.java
+++ 02. Spring Essentials/Exercises/exodia/src/main/java/org/softuni/exodia/domain/models/view/document/DocumentDetailsViewModel.java
@@ -5,9 +5,13 @@ import lombok.NoArgsConstructor;
 import org.softuni.exodia.domain.api.Viewable;
 import org.softuni.exodia.domain.entities.Document;
 
+import java.io.Serializable;
+
 @Getter
 @NoArgsConstructor
-public class DocumentDetailsViewModel implements Viewable<Document> {
+public class DocumentDetailsViewModel implements Viewable<Document>, Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private String id;
     private String title;
