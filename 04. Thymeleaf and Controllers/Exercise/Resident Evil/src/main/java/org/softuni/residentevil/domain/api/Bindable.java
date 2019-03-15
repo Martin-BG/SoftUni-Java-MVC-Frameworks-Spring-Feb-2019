0c10b6e617376e2980aa08c7d4b140d890b3f51f
MartinBG <MartinBG@abv.bg>
2019-03-15 20:04:16 +0200

    domain interfaces

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Bindable.java
@@ -0,0 +1,10 @@
+package org.softuni.residentevil.domain.api;
+
+/**
+ * Bindable model markup interface.
+ * Provides type-safety in generic methods and abstract classes
+ *
+ * @param <E> Entity class to which this Bindable model applies to
+ */
+public interface Bindable<E> {
+}
