6c7ccea25aabc563260ff92d2ccc56feefffc748
MartinBG <MartinBG@abv.bg>
2019-03-15 20:11:18 +0200

    Custom Thymeleaf templating: @Layout + Interceptor

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/annotations/Layout.java
@@ -0,0 +1,55 @@
+package org.softuni.residentevil.annotations;
+
+import org.softuni.residentevil.web.interceptors.ThymeleafLayoutInterceptor;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * Defines Thymeleaf layout template to render annotated view to.
+ * <p>
+ * Can be placed on Controller class or methods (higher priority).
+ * <p>
+ * value is optional and overrides default template set in {@link ThymeleafLayoutInterceptor}
+ * <p>
+ * Set value to {@value #NONE} to prevent modifications by {@link ThymeleafLayoutInterceptor}
+ * <hr>
+ * Sample layout (resources/templates/layouts/default.html):
+ * <pre>{@code <!DOCTYPE html>
+ * <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:th="http://www.thymeleaf.org">
+ * <head>...</head>
+ * <body>
+ *     <header>...</header>
+ *     <main th:insert="${view}"></main>
+ *     <footer>...</footer>
+ * </body>
+ * </html>
+ * }</pre>
+ * Sample view (resources/templates/views/hello.html):
+ * <pre>{@code <th:block xmlns:th="http://www.thymeleaf.org">
+ *     <h1>Hello World!</h1>
+ * </th:block>
+ * }</pre>
+ * Sample controller method:
+ * <pre>{@code     @Layout
+ *     @GetMapping("/hello")
+ *     public String hello() {
+ *         return "hello";
+ *     }}</pre>
+ * <p>
+ * <hr>
+ * Inspired by kolorobot's <a href="https://github.com/kolorobot/thymeleaf-custom-layout">Thymeleaf Custom Layout</a>
+ */
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Layout {
+    String NONE = "none"; // no layout will be used
+
+    @AliasFor("layout")
+    String value() default "";
+
+    @AliasFor("value")
+    String layout() default "";
+}
---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/interceptors/ThymeleafLayoutInterceptor.java
@@ -0,0 +1,136 @@
+package org.softuni.residentevil.web.interceptors;
+
+import org.softuni.residentevil.annotations.Layout;
+import org.springframework.util.Assert;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Modifies view according to {@link Layout} annotation by rendering it into a layout template.
+ * <ul>Default values (can be changed during interceptor's instance build):
+ * <li>Layout file: {@value #DEFAULT_LAYOUT}</li>
+ * <li>View attribute name in layout file, where the view content
+ * to be inserted to: {@value #DEFAULT_VIEW_ATTRIBUTE_NAME}</li>
+ * <li>View file prefix (path to view files, added to incoming view): {@value #DEFAULT_VIEW_PREFIX}</li>
+ * </ul>
+ * <hr>
+ * Inspired by kolorobot's <a href="https://github.com/kolorobot/thymeleaf-custom-layout">Thymeleaf Custom Layout</a>
+ */
+
+public final class ThymeleafLayoutInterceptor extends HandlerInterceptorAdapter {
+
+    private static final String DEFAULT_LAYOUT = "/layouts/default";
+    private static final String DEFAULT_VIEW_ATTRIBUTE_NAME = "view";
+    private static final String DEFAULT_VIEW_PREFIX = "/views/";
+
+    private String defaultLayout;
+    private String viewPrefix;
+    private String viewAttribute;
+
+    private ThymeleafLayoutInterceptor() {
+        defaultLayout = DEFAULT_LAYOUT;
+        viewAttribute = DEFAULT_VIEW_ATTRIBUTE_NAME;
+        viewPrefix = DEFAULT_VIEW_PREFIX;
+    }
+
+    private static boolean isRedirectOrForward(String viewName) {
+        return viewName.startsWith("redirect:") || viewName.startsWith("forward:");
+    }
+
+    private static Layout getMethodOrTypeAnnotation(Object handler) {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            Layout layout = handlerMethod.getMethodAnnotation(Layout.class);
+            if (layout == null) {
+                layout = handlerMethod.getBeanType().getAnnotation(Layout.class);
+            }
+            return layout;
+        }
+        return null;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           ModelAndView modelAndView) {
+        if (modelAndView == null || !modelAndView.hasView()) {
+            return;
+        }
+
+        String originalViewName = modelAndView.getViewName();
+        if (originalViewName == null || isRedirectOrForward(originalViewName)) {
+            return;
+        }
+
+        Layout layout = getMethodOrTypeAnnotation(handler);
+        if (layout == null) {
+            return;
+        }
+
+        String layoutName = getLayoutName(layout);
+        if (Layout.NONE.equals(layoutName)) {
+            return;
+        }
+
+        if (layoutName.isBlank()) {
+            layoutName = defaultLayout;
+        }
+
+        modelAndView.setViewName(layoutName);
+        modelAndView.addObject(viewAttribute, getView(originalViewName));
+    }
+
+    private String getView(String viewName) {
+        return viewPrefix + viewName;
+    }
+
+    private String getLayoutName(Layout layout) {
+        if (layout != null) {
+            return layout.value();
+        }
+        return defaultLayout;
+    }
+
+    public static final class Builder {
+        private ThymeleafLayoutInterceptor toBuild;
+
+        private Builder() {
+            toBuild = new ThymeleafLayoutInterceptor();
+        }
+
+        public Builder withDefaultLayout(String defaultLayout) {
+            toBuild.defaultLayout = defaultLayout;
+            return this;
+        }
+
+        public Builder withViewAttribute(String viewAttribute) {
+            toBuild.viewAttribute = viewAttribute;
+            return this;
+        }
+
+        public Builder withViewPrefix(String viewPrefix) {
+            toBuild.viewPrefix = viewPrefix;
+            return this;
+        }
+
+        public ThymeleafLayoutInterceptor build() {
+            Assert.notNull(toBuild, "Reuse of builder is not allowed");
+            Assert.hasLength(toBuild.defaultLayout, "Default layout cannot be empty or null");
+            Assert.hasLength(toBuild.viewAttribute, "View attribute name cannot be empty or null");
+            Assert.notNull(toBuild.viewPrefix, "View prefix cannot be null");
+
+            ThymeleafLayoutInterceptor built = toBuild;
+            toBuild = null;
+            return built;
+        }
+    }
+}
