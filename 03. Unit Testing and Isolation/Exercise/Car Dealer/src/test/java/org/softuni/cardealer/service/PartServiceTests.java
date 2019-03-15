02f39e9e4da66e42b0ef06da28159deb28564b63
MartinBG <MartinBG@abv.bg>
2019-03-15 12:02:23 +0200

    PartService unit tests

---
+++ 03. Unit Testing and Isolation/Exercise/Car Dealer/src/test/java/org/softuni/cardealer/service/PartServiceTests.java
@@ -0,0 +1,160 @@
+package org.softuni.cardealer.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+import org.softuni.cardealer.domain.entities.Part;
+import org.softuni.cardealer.domain.models.service.PartServiceModel;
+import org.softuni.cardealer.repository.PartRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class PartServiceTests {
+
+    @Mock
+    private PartRepository repository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private PartServiceImpl service;
+
+    @Test
+    public void savePart_validInputData_correctMethodsAndArgumentsUsed() {
+        PartServiceModel model = mock(PartServiceModel.class);
+        Part part = mock(Part.class);
+        Mockito.when(modelMapper.map(model, Part.class)).thenReturn(part);
+        Mockito.when(repository.saveAndFlush(part)).thenReturn(part);
+        Mockito.when(modelMapper.map(part, PartServiceModel.class)).thenReturn(model);
+
+        PartServiceModel result = service.savePart(model);
+
+        Mockito.verify(modelMapper).map(model, Part.class);
+        Mockito.verify(repository).saveAndFlush(part);
+        Mockito.verify(modelMapper).map(part, PartServiceModel.class);
+        Assert.assertEquals(model, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void savePart_nullInput_throwsIllegalArgumentException() {
+        Mockito.when(modelMapper.map(eq(null), any()))
+                .thenThrow(new IllegalArgumentException());
+
+        service.savePart(null);
+    }
+
+    @Test
+    public void editPart_validInputData_correctMethodsAndArgumentsUsed() {
+        Part part = mock(Part.class);
+        PartServiceModel model = mock(PartServiceModel.class);
+        Mockito.when(model.getId()).thenReturn("id");
+        Mockito.when(model.getName()).thenReturn("name");
+        Mockito.when(model.getPrice()).thenReturn(BigDecimal.ONE);
+        Mockito.when(repository.findById(eq("id"))).thenReturn(Optional.of(part));
+        Mockito.when(repository.saveAndFlush(part)).thenReturn(part);
+        Mockito.when(modelMapper.map(part, PartServiceModel.class)).thenReturn(model);
+
+        PartServiceModel result = service.editPart(model);
+
+        Mockito.verify(repository).findById("id");
+        Mockito.verify(part).setName("name");
+        Mockito.verify(part).setPrice(BigDecimal.ONE);
+        Mockito.verify(repository).saveAndFlush(part);
+        Mockito.verify(modelMapper).map(part, PartServiceModel.class);
+        Assert.assertEquals(model, result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void editPart_invalidId_throwsNullPointerException() {
+        PartServiceModel model = mock(PartServiceModel.class);
+        Mockito.when(model.getId()).thenReturn("id");
+        Mockito.when(repository.findById("id")).thenReturn(Optional.empty());
+
+        service.editPart(model);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void editPart_nullInput_throwsNullPointerException() {
+        service.editPart(null);
+    }
+
+    @Test
+    public void deletePart_validId_correctMethodsAndArgumentsUsed() {
+        Part part = mock(Part.class);
+        PartServiceModel model = mock(PartServiceModel.class);
+        Mockito.when(repository.findById("id")).thenReturn(Optional.of(part));
+        Mockito.when(modelMapper.map(part, PartServiceModel.class)).thenReturn(model);
+
+        PartServiceModel result = service.deletePart("id");
+
+        Mockito.verify(repository).findById("id");
+        Mockito.verify(repository).delete(part);
+        Mockito.verify(modelMapper).map(part, PartServiceModel.class);
+        Assert.assertEquals(model, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deletePart_nullInput_throwsIllegalArgumentException() {
+        Mockito.when(repository.findById(null))
+                .thenThrow(new IllegalArgumentException());
+
+        service.deletePart(null);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void deletePart_invalidId_throwsNoSuchElementException() {
+        Mockito.when(repository.findById("id"))
+                .thenReturn(Optional.empty());
+
+        service.deletePart("id");
+    }
+
+    @Test
+    public void findPartById_validId_correctMethodsAndArgumentsUsed() {
+        Part part = mock(Part.class);
+        PartServiceModel model = mock(PartServiceModel.class);
+        Mockito.when(repository.findById("id")).thenReturn(Optional.of(part));
+        Mockito.when(modelMapper.map(part, PartServiceModel.class)).thenReturn(model);
+
+        PartServiceModel result = service.findPartById("id");
+
+        Mockito.verify(repository).findById("id");
+        Mockito.verify(modelMapper).map(part, PartServiceModel.class);
+        Assert.assertEquals(model, result);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void findPartById_nullInput_throwsIllegalArgumentException() {
+        Mockito.when(repository.findById(null))
+                .thenThrow(new IllegalArgumentException());
+
+        service.findPartById(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void findPartById_invalidId_throwsIllegalArgumentException() {
+        Mockito.when(repository.findById("id"))
+                .thenReturn(Optional.empty());
+
+        Mockito.when(modelMapper.map(null, PartServiceModel.class))
+                .thenThrow(new IllegalArgumentException());
+
+        service.findPartById("id");
+    }
+}
