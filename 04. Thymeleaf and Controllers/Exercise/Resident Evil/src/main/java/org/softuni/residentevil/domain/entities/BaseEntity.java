ad58ba0d274a80d8b78b7d08cceb6f8855141395
MartinBG <MartinBG@abv.bg>
2019-03-15 20:05:40 +0200

    Base entities

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/entities/BaseEntity.java
@@ -0,0 +1,31 @@
+package org.softuni.residentevil.domain.entities;
+
+import org.softuni.residentevil.domain.api.Identifiable;
+
+/**
+ * Base Entity class
+ * Defines equals() and hashCode() methods according to best practices by Vlad Mihalcea
+ *
+ * @see <a href="https://vladmihalcea.com/the-best-way-to-implement-equals-hashcode-and-tostring-with-jpa-and-hibernate/">
+ * The best way to implement equals, hashCode, and toString with JPA and Hibernate</a>
+ */
+abstract class BaseEntity<I> implements Identifiable<I> {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        return getId() != null && getId().equals(((Identifiable) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+}
