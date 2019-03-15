a3eb80a040707508878b98c8b5b65be22401f497
MartinBG <MartinBG@abv.bg>
2019-03-15 20:31:45 +0200

    BaseController class

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/controllers/BaseController.java
@@ -0,0 +1,27 @@
+package org.softuni.residentevil.web.controllers;
+
+import lombok.extern.java.Log;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Level;
+
+@Log
+class BaseController {
+
+    protected BaseController() {
+    }
+
+    protected static Optional<UUID> uuid(String id) {
+        try {
+            return Optional.of(UUID.fromString(id));
+        } catch (IllegalArgumentException e) {
+            log.log(Level.SEVERE, "String cannot be converted to UUID: " + id, e);
+            return Optional.empty();
+        }
+    }
+
+    protected static String redirect(String url) {
+        return "redirect:" + url;
+    }
+}
