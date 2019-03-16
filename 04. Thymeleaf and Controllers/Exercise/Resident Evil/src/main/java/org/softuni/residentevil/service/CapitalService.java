db50f114296441d05c23c93eaf420fa6c01565e5
MartinBG <MartinBG@abv.bg>
2019-03-17 00:09:23 +0200

    capital service interface

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/service/CapitalService.java
@@ -0,0 +1,11 @@
+package org.softuni.residentevil.service;
+
+import org.softuni.residentevil.domain.entities.Capital;
+import org.softuni.residentevil.domain.models.view.capital.CapitalSimpleViewModel;
+
+import java.util.List;
+
+public interface CapitalService extends Service<Capital, Long> {
+
+    List<CapitalSimpleViewModel> getCapitals();
+}
