2783ff5d161fc78bae5418a25c8b9c57e7c41fc1
MartinBG <MartinBG@abv.bg>
2019-03-17 00:10:19 +0200

    Virus service interface

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/service/VirusService.java
@@ -0,0 +1,9 @@
+package org.softuni.residentevil.service;
+
+import org.softuni.residentevil.domain.entities.Virus;
+
+import java.util.UUID;
+
+public interface VirusService extends Service<Virus, UUID> {
+
+}
