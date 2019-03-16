cfffd9c3976b896a39e95f692fc55faffbbdd3d3
MartinBG <MartinBG@abv.bg>
2019-03-16 03:53:54 +0200

    CapitalRepository -> findAllSimpleView method with custom query for getting CapitalSimpleViewModel

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/repository/CapitalRepository.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/repository/CapitalRepository.java
@@ -1,12 +1,21 @@
 package org.softuni.residentevil.repository;
 
 import org.softuni.residentevil.domain.entities.Capital;
+import org.softuni.residentevil.domain.models.view.capital.CapitalSimpleViewModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
 
 @Validated
 @Repository
 public interface CapitalRepository extends JpaRepository<Capital, Long> {
 
+    @Query(value = "SELECT " +
+            "NEW org.softuni.residentevil.domain.models.view.capital.CapitalSimpleViewModel(c.id, c.name) " +
+            "FROM Capital AS c " +
+            "ORDER BY c.name ASC")
+    List<CapitalSimpleViewModel> findAllSimpleView();
 }
