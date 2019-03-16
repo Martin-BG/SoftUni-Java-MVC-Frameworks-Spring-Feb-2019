e02fa54f5f024c95a1ca76703b9d4e5ecdd992ac
MartinBG <MartinBG@abv.bg>
2019-03-17 00:12:12 +0200

    show viruses controller

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/controllers/virus/ShowVirusController.java
@@ -0,0 +1,28 @@
+package org.softuni.residentevil.web.controllers.virus;
+
+import org.softuni.residentevil.annotations.Layout;
+import org.softuni.residentevil.domain.models.view.virus.VirusSimpleViewModel;
+import org.softuni.residentevil.service.VirusService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Layout
+@Controller
+@RequestMapping("/viruses")
+public class ShowVirusController {
+
+    private final VirusService virusService;
+
+    public ShowVirusController(VirusService virusService) {
+        this.virusService = virusService;
+    }
+
+    @GetMapping
+    public String get(Model model) {
+        model.addAttribute("viruses", virusService.findAll(VirusSimpleViewModel.class));
+
+        return "virus/all";
+    }
+}
