17867cc376e58b9b34b1291ec7f6922dd760bdb6
MartinBG <MartinBG@abv.bg>
2019-03-16 03:09:15 +0200

    Virus binding model

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/models/binding/virus/VirusBindingModel.java
@@ -0,0 +1,61 @@
+package org.softuni.residentevil.domain.models.binding.virus;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.softuni.residentevil.annotations.composite.virus.*;
+import org.softuni.residentevil.domain.api.Bindable;
+import org.softuni.residentevil.domain.entities.Virus;
+import org.softuni.residentevil.domain.enums.Creator;
+import org.softuni.residentevil.domain.enums.Magnitude;
+import org.softuni.residentevil.domain.enums.Mutation;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class VirusBindingModel implements Bindable<Virus> {
+
+    private String id;
+
+    @ValidVirusName
+    private String name;
+
+    @ValidVirusDescription
+    private String description;
+
+    @ValidVirusSideEffects
+    private String sideEffects;
+
+    @ValidVirusCreator
+    private Creator creator;
+
+    @ValidVirusIsDeadly
+    private Boolean isDeadly;
+
+    @ValidVirusIsCurable
+    private Boolean isCurable;
+
+    @ValidVirusMutation
+    private Mutation mutation;
+
+    @ValidVirusTurnoverRate
+    private Integer turnoverRate;
+
+    @ValidVirusHoursUntilMutation
+    private Integer hoursUntilMutation;
+
+    @ValidVirusMagnitude
+    private Magnitude magnitude;
+
+    @ValidVirusReleasedOn
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate releasedOn;
+
+    @ValidVirusCapitals
+    private List<Long> capIds = new ArrayList<>();
+}
