3f89696a6d2c4a97ce00f264ce986dc30e07afb7
MartinBG <MartinBG@abv.bg>
2019-03-17 00:10:45 +0200

    Virus service

---
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/service/VirusServiceImpl.java
@@ -0,0 +1,32 @@
+package org.softuni.residentevil.service;
+
+import lombok.extern.java.Log;
+import org.modelmapper.ModelMapper;
+import org.softuni.residentevil.domain.entities.Virus;
+import org.softuni.residentevil.repository.VirusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@Log
+@Service
+@Validated
+@Transactional
+@CacheConfig(cacheNames = "viruses")
+public class VirusServiceImpl extends BaseService<Virus, UUID, VirusRepository> implements VirusService {
+
+    @Autowired
+    public VirusServiceImpl(VirusRepository repository, ModelMapper mapper) {
+        super(repository, mapper);
+    }
+
+    @Override
+    protected Logger logger() {
+        return log;
+    }
+}
