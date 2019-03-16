ea17d66db70a619e5c16ea591c0b4f6aee2ab7c3
MartinBG <MartinBG@abv.bg>
2019-03-16 18:00:36 +0200

    CapitalBindingModel: add getter, setter, equals (required for proper binding of multi-select lists in Thymeleaf) and hashCode methods

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/models/binding/capital/CapitalBindingModel.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/models/binding/capital/CapitalBindingModel.java
@@ -1,12 +1,18 @@
 package org.softuni.residentevil.domain.models.binding.capital;
 
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.softuni.residentevil.domain.api.Bindable;
 import org.softuni.residentevil.domain.entities.Capital;
 
 import javax.validation.constraints.NotNull;
 
-@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
 public class CapitalBindingModel implements Bindable<Capital> {
 
     @NotNull
