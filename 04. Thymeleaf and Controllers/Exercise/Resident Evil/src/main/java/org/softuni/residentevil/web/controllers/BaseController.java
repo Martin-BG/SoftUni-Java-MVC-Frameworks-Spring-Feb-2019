5335e50734997ea23678bcffec956b821ba9b13f
MartinBG <MartinBG@abv.bg>
2019-03-18 16:30:43 +0200

    enable text trimming for all input fields, except for those specified in doNotTrimFieldsList.

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/controllers/BaseController.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/controllers/BaseController.java
@@ -1,7 +1,12 @@
 package org.softuni.residentevil.web.controllers;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
+import java.beans.PropertyEditorSupport;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -23,5 +28,39 @@ public class BaseController {
 
     protected static String redirect(String url) {
         return "redirect:" + url;
+    }
+
+    /**
+     * Prevent text modification (like trimming) of specified input fields,
+     * for example "password", "repeatPassword" etc.
+     *
+     * @param binder              {@link WebDataBinder}
+     * @param doNotTrimFieldsList List of field names that should be read without modifications
+     */
+    private static void preventTextModificationForFields(WebDataBinder binder, List<String> doNotTrimFieldsList) {
+        PropertyEditorSupport noTrimPropertyEditor = new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) {
+                super.setValue(text);
+            }
+        };
+
+        doNotTrimFieldsList.forEach(field ->
+                binder.registerCustomEditor(String.class, field, noTrimPropertyEditor));
+    }
+
+    /**
+     * Define modification rules for input fields<br>
+     * Settings are inherited by extending classes
+     *
+     * @param binder {@link WebDataBinder}
+     */
+    @InitBinder
+    private void initBinder(WebDataBinder binder) {
+        // Trim form input texts, empty strings are NOT treated as null
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+
+        List<String> doNotTrimFieldsList = List.of(); // field names, ex. "password"
+        preventTextModificationForFields(binder, doNotTrimFieldsList);
     }
 }
