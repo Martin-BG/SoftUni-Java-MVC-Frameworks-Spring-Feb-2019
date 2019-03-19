16d465fbb618839b981587f365a7992fa51e8aef
MartinBG <MartinBG@abv.bg>
2019-03-19 12:14:22 +0200

    delete virus view page

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/controllers/virus/DeleteVirusController.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/web/controllers/virus/DeleteVirusController.java
@@ -1,31 +1,53 @@
 package org.softuni.residentevil.web.controllers.virus;
 
 import org.softuni.residentevil.annotations.Layout;
+import org.softuni.residentevil.domain.models.binding.virus.VirusBindingModel;
+import org.softuni.residentevil.domain.models.view.capital.CapitalSimpleViewModel;
+import org.softuni.residentevil.service.CapitalService;
 import org.softuni.residentevil.service.VirusService;
-import org.softuni.residentevil.web.controllers.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
+import java.util.List;
 import java.util.UUID;
 
 @Layout
 @Controller
 @RequestMapping("/viruses/delete")
-public class DeleteVirusController extends BaseController {
-
-    private final VirusService service;
+public class DeleteVirusController extends BaseVirusController {
 
     @Autowired
-    public DeleteVirusController(VirusService service) {
-        this.service = service;
+    public DeleteVirusController(VirusService virusService,
+                                 CapitalService capitalService) {
+        super(virusService, capitalService);
+    }
+
+    @ModelAttribute(CAPITALS)
+    public List<CapitalSimpleViewModel> capitalSimpleViewModelList() {
+        return capitalService.getCapitals();
+    }
+
+    @PostMapping
+    public String post(@RequestParam UUID id, Model model) {
+        return virusService
+                .findById(id, VirusBindingModel.class)
+                .map(virus -> {
+                    model.addAttribute(VIRUS, virus);
+                    return "virus/delete";
+                })
+                .orElseThrow();
     }
 
     @DeleteMapping
-    public String get(@RequestParam UUID id) {
-        service.deleteById(id);
+    public String delete(@RequestParam UUID id,
+                         SessionStatus sessionStatus) {
+        sessionStatus.setComplete();
+
+        virusService.deleteById(id);
+
         return redirect("/viruses");
     }
 }
