74ca793f3dc15ff4498a83fa0e616363eb57f641
MartinBG <MartinBG@abv.bg>
2019-03-19 16:22:36 +0200

    Revert "add Serializable to Viewable and (explicitly to document intention by the code) to Bindable interfaces as instances of classes implementing those often end up stored in Session"
    This reverts commit ac6a4545

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Bindable.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Bindable.java
@@ -1,16 +1,10 @@
 package org.softuni.residentevil.domain.api;
 
-import java.io.Serializable;
-
 /**
  * Bindable model markup interface.
- * Provides type-safety in generic methods and abstract classes
- * <p>
- * Extends {@link Viewable} as Bindable models can be requested by Controllers for edit.<br>
- * Extends {@link Serializable} (also through {@link Viewable}) as instances of implementing
- * classes could end up stored in Session
- *
+ * Provides type-safety in generic methods and abstract classes<br>
+ * Extends {@link Viewable} as Bindable models can be requested by Controllers for edit.
  * @param <E> Entity class to which this Bindable model applies to
  */
-public interface Bindable<E> extends Viewable<E>, Serializable {
+public interface Bindable<E> extends Viewable<E> {
 }
--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Viewable.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Viewable.java
@@ -1,15 +1,10 @@
 package org.softuni.residentevil.domain.api;
 
-import java.io.Serializable;
-
 /**
  * Viewable model markup interface.
  * Provides type-safety in generic methods and abstract classes
- * <p>
- * Extends {@link Serializable} as instances of implementing
- * classes could end up stored in Session
  *
  * @param <E> Entity class to which this Viewable model applies to
  */
-public interface Viewable<E> extends Serializable {
+public interface Viewable<E> {
 }
