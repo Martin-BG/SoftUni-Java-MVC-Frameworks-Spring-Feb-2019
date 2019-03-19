dc7f8b4b2307ec296cbb8a7badd016c90a46945d
MartinBG <MartinBG@abv.bg>
2019-03-19 16:22:55 +0200

    Revert "add Serializable to Identifiable as instances of implementing classes (entities) could end in Session storage or in Cache"
    This reverts commit 9237713c

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Identifiable.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Identifiable.java
@@ -1,17 +1,12 @@
 package org.softuni.residentevil.domain.api;
 
-import java.io.Serializable;
-
 /**
  * Identifiable markup interface for entities or models.
  * Provides type-safety in generic methods and abstract classes.
- * <p>
- * Extends {@link Serializable} as instances of implementing
- * classes could end in Session storage or in Cache
  *
  * @param <I> ID type of the entity class
  */
-public interface Identifiable<I> extends Serializable {
+public interface Identifiable<I> {
 
     I getId();
 }
