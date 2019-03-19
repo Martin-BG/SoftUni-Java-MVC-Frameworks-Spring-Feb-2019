ac6a45450122554b608ec0193e3fda5857202269
MartinBG <MartinBG@abv.bg>
2019-03-19 12:49:48 +0200

    add Serializable to Viewable and (explicitly to document intention by the code) to Bindable interfaces as instances of classes implementing those often end up stored in Session

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Bindable.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Bindable.java
@@ -1,10 +1,16 @@
 package org.softuni.residentevil.domain.api;
 
+import java.io.Serializable;
+
 /**
  * Bindable model markup interface.
- * Provides type-safety in generic methods and abstract classes<br>
- * Extends {@link Viewable} as Bindable models can be requested by Controllers for edit.
+ * Provides type-safety in generic methods and abstract classes
+ * <p>
+ * Extends {@link Viewable} as Bindable models can be requested by Controllers for edit.<br>
+ * Extends {@link Serializable} (also through {@link Viewable}) as instances of implementing
+ * classes could end up stored in Session
+ *
  * @param <E> Entity class to which this Bindable model applies to
  */
-public interface Bindable<E> extends Viewable<E> {
+public interface Bindable<E> extends Viewable<E>, Serializable {
 }
--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Viewable.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Viewable.java
@@ -1,10 +1,15 @@
 package org.softuni.residentevil.domain.api;
 
+import java.io.Serializable;
+
 /**
  * Viewable model markup interface.
  * Provides type-safety in generic methods and abstract classes
+ * <p>
+ * Extends {@link Serializable} as instances of implementing
+ * classes could end up stored in Session
  *
  * @param <E> Entity class to which this Viewable model applies to
  */
-public interface Viewable<E> {
+public interface Viewable<E> extends Serializable {
 }
