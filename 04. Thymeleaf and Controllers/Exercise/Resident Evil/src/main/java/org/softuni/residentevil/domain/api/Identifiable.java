9237713c30219bc2c93c5e3fd04ab832b3ebc44f
MartinBG <MartinBG@abv.bg>
2019-03-19 12:56:35 +0200

    add Serializable to Identifiable as instances of implementing classes (entities) could end in Session storage or in Cache

--- 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Identifiable.java
+++ 04. Thymeleaf and Controllers/Exercise/Resident Evil/src/main/java/org/softuni/residentevil/domain/api/Identifiable.java
@@ -1,12 +1,17 @@
 package org.softuni.residentevil.domain.api;
 
+import java.io.Serializable;
+
 /**
  * Identifiable markup interface for entities or models.
  * Provides type-safety in generic methods and abstract classes.
+ * <p>
+ * Extends {@link Serializable} as instances of implementing
+ * classes could end in Session storage or in Cache
  *
  * @param <I> ID type of the entity class
  */
-public interface Identifiable<I> {
+public interface Identifiable<I> extends Serializable {
 
     I getId();
 }
