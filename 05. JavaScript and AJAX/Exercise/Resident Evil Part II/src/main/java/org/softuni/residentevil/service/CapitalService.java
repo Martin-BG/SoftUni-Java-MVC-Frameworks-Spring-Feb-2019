14af2c45d0f944602bcb7878d74f97621c62d9e5
MartinBG <MartinBG@abv.bg>
2019-03-24 04:40:48 +0200

    add getCapitalsDetailed method to CapitalService, refactoring

--- 05. JavaScript and AJAX/Exercise/Resident Evil Part II/src/main/java/org/softuni/residentevil/service/CapitalService.java
+++ 05. JavaScript and AJAX/Exercise/Resident Evil Part II/src/main/java/org/softuni/residentevil/service/CapitalService.java
@@ -1,6 +1,7 @@
 package org.softuni.residentevil.service;
 
 import org.softuni.residentevil.domain.entities.Capital;
+import org.softuni.residentevil.domain.models.projections.capital.CapitalAllProjection;
 import org.softuni.residentevil.domain.models.view.capital.CapitalSimpleViewModel;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface CapitalService extends Service<Capital, Long> {
 
     List<CapitalSimpleViewModel> getCapitals();
+
+    List<CapitalAllProjection> getCapitalsDetailed();
 }
--- 05. JavaScript and AJAX/Exercise/Resident Evil Part II/src/main/java/org/softuni/residentevil/service/CapitalServiceImpl.java
+++ 05. JavaScript and AJAX/Exercise/Resident Evil Part II/src/main/java/org/softuni/residentevil/service/CapitalServiceImpl.java
@@ -3,10 +3,12 @@ package org.softuni.residentevil.service;
 import lombok.extern.java.Log;
 import org.modelmapper.ModelMapper;
 import org.softuni.residentevil.domain.entities.Capital;
+import org.softuni.residentevil.domain.models.projections.capital.CapitalAllProjection;
 import org.softuni.residentevil.domain.models.view.capital.CapitalSimpleViewModel;
 import org.softuni.residentevil.repository.CapitalRepository;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -18,10 +20,13 @@ import java.util.logging.Logger;
 @Service
 @Validated
 @Transactional
-@CacheConfig(cacheNames = "capitals")
+@CacheConfig(cacheNames = CapitalServiceImpl.CAPITALS)
 public class CapitalServiceImpl extends BaseService<Capital, Long, CapitalRepository> implements CapitalService {
 
-    protected CapitalServiceImpl(CapitalRepository repository, ModelMapper mapper) {
+    public static final String CAPITALS = "capitals";
+    private static final Sort SORT_ASC_BY_NAME = Sort.by(Sort.Direction.ASC, "name");
+
+    public CapitalServiceImpl(CapitalRepository repository, ModelMapper mapper) {
         super(repository, mapper);
     }
 
@@ -35,5 +40,12 @@ public class CapitalServiceImpl extends BaseService<Capital, Long, CapitalReposi
     @Transactional(readOnly = true)
     public List<CapitalSimpleViewModel> getCapitals() {
         return repository.findAllSimpleView();
+    }
+
+    @Override
+    @Cacheable(sync = true)
+    @Transactional(readOnly = true)
+    public List<CapitalAllProjection> getCapitalsDetailed() {
+        return repository.findAllProjectedBy(CapitalAllProjection.class, SORT_ASC_BY_NAME);
     }
 }
