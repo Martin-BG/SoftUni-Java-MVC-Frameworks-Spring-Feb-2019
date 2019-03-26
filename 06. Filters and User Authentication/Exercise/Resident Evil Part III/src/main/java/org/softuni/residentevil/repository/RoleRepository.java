c908f19558af4a71ab3efad9b6697486e4258bbe
MartinBG <MartinBG@abv.bg>
2019-03-26 16:52:24 +0200

    refactored

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/repository/RoleRepository.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/repository/RoleRepository.java
@@ -2,16 +2,14 @@ package org.softuni.residentevil.repository;
 
 import org.softuni.residentevil.domain.entities.Role;
 import org.softuni.residentevil.domain.validation.annotations.composite.role.ValidRoleEntityAuthority;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Validated
 @Repository
-public interface RoleRepository extends JpaRepository<Role, UUID> {
+public interface RoleRepository extends GenericRepository<Role, Long> {
 
     Optional<Role> findRoleByAuthority(@ValidRoleEntityAuthority String authority);
 }
