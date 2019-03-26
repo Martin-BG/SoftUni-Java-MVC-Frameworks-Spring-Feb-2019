d3beb9ef3900cfe415b56b21647c22dcb1f01dcf
MartinBG <MartinBG@abv.bg>
2019-03-26 14:38:04 +0200

    @ValidUserEntityPassword

---
+++ 06. Filters, Interceptors and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/domain/validation/annotations/composite/user/ValidUserEntityPassword.java
@@ -0,0 +1,29 @@
+package org.softuni.residentevil.domain.validation.annotations.composite.user;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Cannot be null
+ */
+
+@NotNull
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface ValidUserEntityPassword {
+
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
