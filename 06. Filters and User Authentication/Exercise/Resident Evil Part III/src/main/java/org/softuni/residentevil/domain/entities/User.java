9a40eb98f7accd8faaef56da52bb8859f4db71b5
MartinBG <MartinBG@abv.bg>
2019-03-26 14:50:02 +0200

    refactoring

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/domain/entities/User.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/domain/entities/User.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,27 +18,32 @@ import java.util.Set;
 @Table(name = "users")
 @NamedQuery(name = "User.findUserEager",
         query = "SELECT u FROM User u LEFT JOIN FETCH u.authorities AS a WHERE u.username = :username")
-public class User extends BaseUuidEntity implements UserDetails, Serializable {
+public class User extends BaseUuidEntity implements UserDetails {
 
     private static final long serialVersionUID = 1L;
-    boolean isAccountNonLocked = true;
-    boolean isAccountNonExpired = true;
-    boolean isCredentialsNonExpired = true;
-    boolean isEnabled = true;
+
+    @ValidUserUsername
+    @Column(unique = true, nullable = false, updatable = false, length = ValidUserUsername.MAX_LENGTH)
+    private String username;
+
+    @ValidUserEntityPassword
+    @Column(nullable = false)
+    private String password;
+
+    @ValidUserEmail
+    @Column(unique = true, nullable = false, length = ValidUserEmail.MAX_LENGTH)
+    private String email;
+
     @NotEmpty
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id")})
-    Set<Role> authorities = new HashSet<>();
-    @ValidUserUsername
-    @Column(nullable = false, unique = true, updatable = false, length = ValidUserUsername.MAX_LENGTH)
-    private String username;
-    @ValidUserEntityPassword
-    @Column(nullable = false)
-    private String password;
-    @ValidUserEmail
-    @Column(nullable = false, unique = true, length = ValidUserEmail.MAX_LENGTH)
-    private String email;
+    private Set<Role> authorities = new HashSet<>();
+
+    private boolean isAccountNonLocked = true;
+    private boolean isAccountNonExpired = true;
+    private boolean isCredentialsNonExpired = true;
+    private boolean isEnabled = true;
 }
