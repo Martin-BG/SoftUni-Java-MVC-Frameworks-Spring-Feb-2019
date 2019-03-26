44254cba59d1f65d046058c67ac4e8e26341c03e
MartinBG <MartinBG@abv.bg>
2019-03-26 16:52:41 +0200

    refactored

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/repository/UserRepository.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package org.softuni.residentevil.repository;
 
 import org.softuni.residentevil.domain.entities.User;
+import org.softuni.residentevil.domain.validation.annotations.composite.user.ValidUserEmail;
 import org.softuni.residentevil.domain.validation.annotations.composite.user.ValidUserUsername;
 import org.springframework.stereotype.Repository;
 import org.springframework.validation.annotation.Validated;
@@ -13,4 +14,8 @@ import java.util.UUID;
 public interface UserRepository extends GenericRepository<User, UUID> {
 
     Optional<User> findUserEager(@ValidUserUsername String username);
+
+    long countByUsername(@ValidUserUsername String username);
+
+    long countByEmail(@ValidUserEmail String email);
 }
