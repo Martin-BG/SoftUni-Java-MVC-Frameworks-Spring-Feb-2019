3e916002285e07a9aaaf59909252668bd9c67192
MartinBG <MartinBG@abv.bg>
2019-03-26 14:34:40 +0200

    UserRepository

---
+++ 06. Filters, Interceptors and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package org.softuni.residentevil.repository;
+
+import org.softuni.residentevil.domain.entities.User;
+import org.softuni.residentevil.domain.validation.annotations.composite.user.ValidUserUsername;
+import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Validated
+@Repository
+public interface UserRepository extends GenericRepository<User, UUID> {
+
+    Optional<User> findUserEager(@ValidUserUsername String username);
+}
