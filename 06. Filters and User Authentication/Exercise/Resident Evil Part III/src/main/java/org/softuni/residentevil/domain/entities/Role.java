20b3f087b04c7d59c1b1cfe886348df54c3470ca
MartinBG <MartinBG@abv.bg>
2019-03-26 14:48:00 +0200

    refactoring

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/domain/entities/Role.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/domain/entities/Role.java
@@ -9,18 +9,17 @@ import org.springframework.security.core.GrantedAuthority;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.io.Serializable;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "roles")
-public class Role extends BaseLongEntity implements GrantedAuthority, Serializable {
+public class Role extends BaseLongEntity implements GrantedAuthority {
 
     private static final long serialVersionUID = 1L;
 
     @ValidRoleEntityAuthority
-    @Column(nullable = false, unique = true, length = ValidRoleEntityAuthority.MAX_LENGTH)
+    @Column(unique = true, nullable = false, length = ValidRoleEntityAuthority.MAX_LENGTH)
     private String authority;
 }
