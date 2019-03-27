408d758a83c55764fde83005513f1746fbbdb4b8
MartinBG <MartinBG@abv.bg>
2019-03-27 05:33:51 +0200

    Index -> redirect to Home for logged users (better user experience)

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/web/controllers/IndexController.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/web/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.softuni.residentevil.web.annotations.Layout;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Layout
 @Controller
 public class IndexController extends BaseController {
@@ -12,7 +14,10 @@ public class IndexController extends BaseController {
     private static final String VIEW_INDEX = "index";
 
     @GetMapping(WebConfig.URL_INDEX)
-    public String index() {
+    public String index(Principal principal) {
+        if (principal != null) {
+            return redirect(WebConfig.URL_USER_HOME);
+        }
         return VIEW_INDEX;
     }
 }
