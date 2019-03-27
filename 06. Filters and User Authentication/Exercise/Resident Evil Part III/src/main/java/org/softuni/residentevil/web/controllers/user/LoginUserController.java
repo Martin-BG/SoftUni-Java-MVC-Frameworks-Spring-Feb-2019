3e5b8428125e422773486da4e0067d5dab5bcf8f
MartinBG <MartinBG@abv.bg>
2019-03-27 06:30:18 +0200

    prevent password trim on User Login

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/web/controllers/user/LoginUserController.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/web/controllers/user/LoginUserController.java
@@ -7,12 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Layout
 @Controller
 @RequestMapping(WebConfig.URL_USER_LOGIN)
 public class LoginUserController extends BaseController {
 
     private static final String VIEW_REGISTER = "user/login";
+
+    @Override
+    protected List<String> getUnmodifiedTextFieldsList() {
+        return List.of("password");
+    }
 
     @GetMapping
     public String get() {
