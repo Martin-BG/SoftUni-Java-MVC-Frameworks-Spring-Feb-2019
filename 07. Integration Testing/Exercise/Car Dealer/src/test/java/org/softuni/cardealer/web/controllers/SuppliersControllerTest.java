ee0c2be422b5a8757d607bf16379eea4da1a0c77
MartinBG <MartinBG@abv.bg>
2019-03-29 10:33:58 +0200

    Suppliers controller tests

--- 07. Integration Testing/Exercise/Car Dealer/src/test/java/org/softuni/cardealer/web/controllers/SuppliersControllerTest.java
+++ 07. Integration Testing/Exercise/Car Dealer/src/test/java/org/softuni/cardealer/web/controllers/SuppliersControllerTest.java
@@ -35,7 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class SuppliersControllerTest {
 
-    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+    private static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
             MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
             Charset.forName("utf8"));
