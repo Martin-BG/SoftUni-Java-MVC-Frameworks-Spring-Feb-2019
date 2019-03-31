d04b33cfa25ffec143b16f9b40699fa701c35b92
MartinBG <MartinBG@abv.bg>
2019-03-31 06:18:39 +0300

    updateRole and allUsers methods

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/service/UserService.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/service/UserService.java
@@ -2,13 +2,20 @@ package org.softuni.residentevil.service;
 
 import org.softuni.residentevil.domain.entities.User;
 import org.softuni.residentevil.domain.models.binding.user.UserRegisterBindingModel;
+import org.softuni.residentevil.domain.models.binding.user.UserRoleBindingModel;
+import org.softuni.residentevil.domain.models.view.user.UserViewModel;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 
 public interface UserService extends Service<User, UUID>, UserDetailsService {
 
     void registerUser(@NotNull @Valid UserRegisterBindingModel bindingModel);
+
+    List<UserViewModel> allUsers();
+
+    void updateRole(@NotNull @Valid UserRoleBindingModel userRoleBindingModel);
 }
