5c4cc1ecdebf0208120086cd9ed0de7e9a9b7d61
MartinBG <MartinBG@abv.bg>
2019-04-07 17:26:21 +0300

    URL constants refactoring, changed virus base URL from "viruses" to a more proper "virus" name

--- 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/config/WebConfig.java
+++ 06. Filters and User Authentication/Exercise/Resident Evil Part III/src/main/java/org/softuni/residentevil/config/WebConfig.java
@@ -10,17 +10,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final String VIRUS_BASE_URL = "/virus";
+    public static final String URL_VIRUS_ALL = VIRUS_BASE_URL;
+
     public static final String URL_INDEX = "/";
-    public static final String URL_VIRUS_ALL = "/viruses";
-    public static final String URL_VIRUS_ADD = "/viruses/add";
-    public static final String URL_VIRUS_DELETE = "/viruses/delete";
-    public static final String URL_VIRUS_EDIT = "/viruses/edit";
+    public static final String URL_VIRUS_ADD = VIRUS_BASE_URL + "/add";
+    public static final String URL_VIRUS_DELETE = VIRUS_BASE_URL + "/delete";
+    public static final String URL_VIRUS_EDIT = VIRUS_BASE_URL + "/edit";
+    private static final String USER_BASE_URL = "/user";
     public static final String URL_ALL = "/all";
-    public static final String URL_USER_REGISTER = "/user/register";
-    public static final String URL_USER_LOGIN = "/user/login";
-    public static final String URL_USER_LOGOUT = "/user/logout";
-    public static final String URL_USER_HOME = "/user/home";
-    public static final String URL_USER_ALL = "/user";
+    public static final String URL_USER_REGISTER = USER_BASE_URL + "/register";
+    public static final String URL_USER_LOGIN = USER_BASE_URL + "/login";
+    public static final String URL_USER_LOGOUT = USER_BASE_URL + "/logout";
+    public static final String URL_USER_HOME = USER_BASE_URL + "/home";
+    public static final String URL_USER_ALL = USER_BASE_URL;
     public static final String URL_ERROR_UNAUTHORIZED = "/unauthorized";
 
     public static final String REST_API_CAPITAL = "/api/capital";
